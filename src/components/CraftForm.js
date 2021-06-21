33a2baf6e2cd8d41521aa364c9f8c8a6d41cdfe2
Michelle Ranly <meranly8@gmail.com>
2021-06-20 23:49:43 -0500

    Setup CraftForm class component

---
+++ src/components/CraftForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class CraftForm extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default CraftForm

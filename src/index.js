eae710dbb902dc4291a54ec9ad5d8abb62eaa4b1
Michelle Ranly <meranly8@gmail.com>
2021-06-21 23:00:38 -0500

    Imported store & thunk elements; wrapped App in <Provider>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import craftReducer from './reducers/manageCrafts'
+
+
 // import reportWebVitals from './reportWebVitals'
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    </Provider>,
   document.getElementById('root')
 )
 

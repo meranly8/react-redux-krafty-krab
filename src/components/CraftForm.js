f24a539b3d4f38fb91e60be18449964442676aed
Michelle Ranly <meranly8@gmail.com>
2021-06-21 19:32:53 -0500

    Built CraftForm class component & rendered <form>

--- src/components/CraftForm.js
+++ src/components/CraftForm.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react'
 
 class CraftForm extends Component {
+    
     render(){
         return (
             <div>
-
+                <p><label>Name &nbsp;
+                    <input name="name" required/>
+                </label></p>
+                <p><label>Craft Type &nbsp;
+                    <select name="craft-type" required>
+                        <option selected="Select"></option>
+                        <option value="bracelet">Bracelet</option>
+                        <option value="embroidery">Embroidery</option>
+                        <option value="knit">Knit</option>
+                        <option value="painting">Painting</option>
+                        <option value="pom-poms">Pom Poms</option>
+                    </select>
+                </label></p>
+                <p><label>Description &nbsp;
+                    <textarea name="description" />
+                </label></p>
+                <p><label>Date Started &nbsp;
+                    <input type="date" name="date-started" />
+                </label></p>
+                <p><label>Date Completed &nbsp;
+                    <input type="date" name="date-completed" />
+                </label></p>
+                <p><label>Date Sold &nbsp;
+                    <input type="date" name="date-sold" />
+                </label></p>
+                <p><label>Price $
+                    <input type="number" name="price" min="0"/>
+                </label></p>
+                <p><label>Notes &nbsp;
+                    <textarea name="notes" />
+                </label></p>
+                <input type="submit"/><br /><br /><br />
             </div>
         )
     }
-
 }
 
 export default CraftForm

ffee5ff78ce46d634c54d5cef93dafa64df34f40
Michelle Ranly <meranly8@gmail.com>
2021-06-22 00:05:01 -0500

    Added loading: false to new SET_CRAFTS returned state

--- src/reducers/manageCrafts.js
+++ src/reducers/manageCrafts.js
@@ -1,7 +1,7 @@
 const manageCrafts = (state = {crafts: [], loading: false}, action) => {
     switch(action.type){
         case 'SET_CRAFTS':
-            return {...state, crafts: action.payload}
+        return {...state, crafts: action.payload, loading: false}
         case 'LOADING':
             return {...state, loading: true}
         default:

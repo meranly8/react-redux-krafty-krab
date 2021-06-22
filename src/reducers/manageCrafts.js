ceca45a9c1d6c7ce4acae33dac8ec3fb565ae035
Michelle Ranly <meranly8@gmail.com>
2021-06-22 01:09:01 -0500

    Added ADD_CRAFT case to manageCrafts reducer

--- src/reducers/manageCrafts.js
+++ src/reducers/manageCrafts.js
@@ -4,6 +4,8 @@ const manageCrafts = (state = {crafts: [], loading: false}, action) => {
         return {...state, crafts: action.payload, loading: false}
         case 'LOADING':
             return {...state, loading: true}
+        case 'ADD_CRAFT':
+            return {...state, crafts: [...state.crafts, action.payload] }
         default:
             return state
     }

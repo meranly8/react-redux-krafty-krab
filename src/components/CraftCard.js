730259570e80ee154f92e344771aed6c58b0fe21
Michelle Ranly <meranly8@gmail.com>
2021-06-23 00:32:15 -0500

    Reformatted CraftCard

--- src/components/CraftCard.js
+++ src/components/CraftCard.js
@@ -5,7 +5,7 @@ import { deleteCraft } from '../actions/craftActions'
 const CraftCard = props => {
     return (
         <div>
-            <h4>{props.craft.name} ({props.craft.craft_type}) <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button></h4>
+            <h5>{props.craft.name} ({props.craft.craft_type}) <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button></h5>
         </div>
     )
 }

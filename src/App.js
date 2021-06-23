69f5b30d4d283e75ed156b7ad10b187140fc09d1
Michelle Ranly <meranly8@gmail.com>
2021-06-22 23:55:43 -0500

    Imported react-router-dom elements & implemented Router & NavBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import CraftsContainer from './containers/Crafts'
+import CraftForm from './components/CraftForm'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Krafty Krab</h1>
-      </header>
-      <section>
-        < CraftsContainer />
-      </section>
+        < Router>
+        <header className="App-header">
+          <h1>Krafty Krab</h1>
+          < NavBar />
+        </header>
+        <main>
+          < Switch>
+            < Route exact path="/crafts/new"><CraftForm /></ Route>
+            < Route  path="/crafts"><CraftsContainer /></ Route>
+          </ Switch>
+        </main>
+      </ Router>
     </div>
+    
   )
 }
 

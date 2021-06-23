75e00868ee0ad739360cf86e09088ba066dca1a8
Michelle Ranly <meranly8@gmail.com>
2021-06-23 01:04:55 -0500

    Added routing to Crafts container for the craft stages & defined renderCraftsByStage()

--- src/containers/Crafts.js
+++ src/containers/Crafts.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { fetchCrafts } from '../actions/craftActions'
 import CraftCard from '../components/CraftCard'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import NavBarCrafts from '../components/NavBarCrafts'
+// import Backlog from '../components/Backlog'
 
 class Crafts extends Component {
     componentDidMount() {
@@ -13,32 +16,48 @@ class Crafts extends Component {
         return craftsByType.map(craft => <CraftCard key={craft.id} craft={craft}/>)
     }
 
+    renderCraftsByStage = stage => {
+        const craftsByStage = this.props.crafts.filter(craft => craft[stage] === true)
+        return craftsByStage.map(craft => <CraftCard key={craft.id} craft={craft}/>)
+    }
+
     render(){
         return (
             <div>
-                {this.props.loading ? <h3>Loading</h3> : null}
-                <ul>
-                    <h4><u>Bracelets</u> ({this.renderCraftsByType("Bracelet").length})</h4>
-                        <ul>
-                            {this.renderCraftsByType("Bracelet")}
-                        </ul>
-                    <h4><u>Embroideries</u> ({this.renderCraftsByType("Embroidery").length})</h4>
-                        <ul>
-                            {this.renderCraftsByType("Embroidery")}
-                        </ul>
-                    <h4><u>Knits</u> ({this.renderCraftsByType("Knit").length})</h4>
-                        <ul>
-                            {this.renderCraftsByType("Knit")}
-                        </ul>
-                    <h4><u>Paintings</u> ({this.renderCraftsByType("Painting").length})</h4>
-                        <ul>
-                            {this.renderCraftsByType("Painting")}
-                        </ul>
-                    <h4><u>Pom Poms</u> ({this.renderCraftsByType("Pom Poms").length})</h4>
-                        <ul>
-                            {this.renderCraftsByType("Pom Poms")}
-                        </ul>
-                </ul>
+                < Router >
+                    < NavBarCrafts />
+                    {this.props.loading ? <h3>Loading</h3> : null}
+                    < Switch >
+                        < Route exact path="/crafts">
+                            <ul>
+                                <h4><u>Bracelets</u> ({this.renderCraftsByType("Bracelet").length})</h4>
+                                    <ul>
+                                        {this.renderCraftsByType("Bracelet")}
+                                    </ul>
+                                <h4><u>Embroideries</u> ({this.renderCraftsByType("Embroidery").length})</h4>
+                                    <ul>
+                                        {this.renderCraftsByType("Embroidery")}
+                                    </ul>
+                                <h4><u>Knits</u> ({this.renderCraftsByType("Knit").length})</h4>
+                                    <ul>
+                                        {this.renderCraftsByType("Knit")}
+                                    </ul>
+                                <h4><u>Paintings</u> ({this.renderCraftsByType("Painting").length})</h4>
+                                    <ul>
+                                        {this.renderCraftsByType("Painting")}
+                                    </ul>
+                                <h4><u>Pom Poms</u> ({this.renderCraftsByType("Pom Poms").length})</h4>
+                                    <ul>
+                                        {this.renderCraftsByType("Pom Poms")}
+                                    </ul>
+                            </ul>
+                        </ Route >
+                        < Route exact path="/crafts/backlog">{this.renderCraftsByStage("backlog")}</Route>
+                        < Route exact path="/crafts/wip">{this.renderCraftsByStage("wip")}</Route>
+                        < Route exact path="/crafts/inventory">{this.renderCraftsByStage("inventory")}</Route>
+                        < Route exact path="/crafts/sold">{this.renderCraftsByStage("sold")}</Route>
+                    </ Switch >
+                </ Router >
             </div>
         )
     }

2ddd4f8f08e1c6875c0d222a12ac0b30e7315964
Michelle Ranly <meranly8@gmail.com>
2021-06-23 00:05:07 -0500

    Created NavBar cfunctional component w/ Links

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return(
+        <div>
+            <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;
+            <Link to="/crafts">All Crafts</Link>&nbsp;&nbsp;&nbsp;
+            <Link to="/crafts/new">New Craft</Link>&nbsp;&nbsp;&nbsp;
+        </div>
+    )
+}
+
+export default NavBar

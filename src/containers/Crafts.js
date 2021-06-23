7256125fdad8cdf79efcb7bad353c1aa30f2fa60
Michelle Ranly <meranly8@gmail.com>
2021-06-23 00:35:49 -0500

    Updated Crafts container to render all crafts by craft_type

--- src/containers/Crafts.js
+++ src/containers/Crafts.js
@@ -8,15 +8,36 @@ class Crafts extends Component {
         this.props.fetchCrafts()
     }
 
-    renderCrafts = () => {
-        return this.props.crafts.map(craft => <CraftCard key={craft.id} craft={craft}/>)
+    renderCraftsByType = (craft_type) => {
+        const craftsByType = this.props.crafts.filter(craft => craft.craft_type === craft_type)
+        return craftsByType.map(craft => <CraftCard key={craft.id} craft={craft}/>)
     }
 
     render(){
         return (
             <div>
+                {this.props.loading ? <h3>Loading</h3> : null}
                 <ul>
-                    {this.props.loading ? <h3>Loading</h3> : this.renderCrafts()}
+                    <h4><u>Bracelets</u> ({this.renderCraftsByType("Bracelet").length})</h4>
+                        <ul>
+                            {this.renderCraftsByType("Bracelet")}
+                        </ul>
+                    <h4><u>Embroideries</u> ({this.renderCraftsByType("Embroidery").length})</h4>
+                        <ul>
+                            {this.renderCraftsByType("Embroidery")}
+                        </ul>
+                    <h4><u>Knits</u> ({this.renderCraftsByType("Knit").length})</h4>
+                        <ul>
+                            {this.renderCraftsByType("Knit")}
+                        </ul>
+                    <h4><u>Paintings</u> ({this.renderCraftsByType("Painting").length})</h4>
+                        <ul>
+                            {this.renderCraftsByType("Painting")}
+                        </ul>
+                    <h4><u>Pom Poms</u> ({this.renderCraftsByType("Pom Poms").length})</h4>
+                        <ul>
+                            {this.renderCraftsByType("Pom Poms")}
+                        </ul>
                 </ul>
             </div>
         )

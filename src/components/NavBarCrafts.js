980d08dc1decf21957353d6cefb218c3e75ec72c
Michelle Ranly <meranly8@gmail.com>
2021-06-24 00:36:18 -0500

    Changed Links to NavLink & added style/activeClassName

--- src/components/NavBarCrafts.js
+++ src/components/NavBarCrafts.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBarCrafts = () => {
+    const style = {
+        textDecoration: 'none', color: 'dark gray'
+    }
+    
     return(
-        <div>
-            <Link to="/crafts/">All</Link>&nbsp;&nbsp;&nbsp;
-            <Link to="/crafts/backlog">Backlog</Link>&nbsp;&nbsp;&nbsp;
-            <Link to="/crafts/wip">Work In Progress</Link>&nbsp;&nbsp;&nbsp;
-            <Link to="/crafts/inventory">Inventory</Link>&nbsp;&nbsp;&nbsp;
-            <Link to="/crafts/sold">Sold</Link>&nbsp;&nbsp;&nbsp;
+        <div className="nav-bar-craft">
+            <NavLink to="/crafts/" exact style={style} activeClassName="current">All</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/backlog" style={style} activeClassName="current">Backlog</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/wip" style={style} activeClassName="current">Work In Progress</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/inventory" style={style} activeClassName="current">Inventory</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/sold" style={style} activeClassName="current">Sold</NavLink>&nbsp;&nbsp;&nbsp;
         </div>
     )
 }

965e14d2e0303721575f8b64f8a835293617492c
Michelle Ranly <meranly8@gmail.com>
2021-06-24 00:58:42 -0500

    Updated header content

--- src/components/HeaderMain.js
+++ src/components/HeaderMain.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const HeaderMain = () => {
-    // const currentDateTime = new Date()
-
     return(
         <div>
             <h1>Krafty Krab</h1>
-            <small>{new Date().toLocaleString() + ''}</small>
+            <small>Currently: {new Date().toLocaleString() + ''}</small>
         </div>
     )
 }

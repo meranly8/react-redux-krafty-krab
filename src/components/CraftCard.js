cbdb92a1349afa73ee2e603f7034559a5b6a1450
Michelle Ranly <meranly8@gmail.com>
2021-06-24 02:38:18 -0500

    Added additional content to display in CraftCard

--- src/components/CraftCard.js
+++ src/components/CraftCard.js
@@ -5,7 +5,7 @@ import { deleteCraft } from '../actions/craftActions'
 const CraftCard = props => {
     return (
         <div>
-            <h5>{props.craft.name} ({props.craft.craft_type}) <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button></h5>
+            <h5>{props.craft.name} ({props.craft.craft_type})</h5>
         </div>
     )
 }

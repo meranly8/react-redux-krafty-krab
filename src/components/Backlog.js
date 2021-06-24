c800f55b7a34f125be0db5005e9f06e44369490d
Michelle Ranly <meranly8@gmail.com>
2021-06-24 02:42:15 -0500

    Created Backlog component rendering BacklogCards

---
+++ src/components/Backlog.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BacklogCard from '../components/BacklogCard'
+import { fetchCrafts } from '../actions/craftActions'
+
+class Backlog extends Component {
+    renderBacklogCards = () => {
+        const backlog = this.props.crafts.filter(craft => craft.backlog === true)
+        const sorted = backlog.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+
+        return sorted.map(craft => < BacklogCard key={craft.id} craft={craft}/>)
+    }
+
+    render() {
+        return (
+            <div>
+                <section>
+                    <h3><u>Most Recent Ideas</u></h3>
+                    {this.renderBacklogCards()}
+                </section>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        crafts: state.crafts,
+        loading: state.loading
+    }
+}
+
+export default connect(mapStateToProps, { fetchCrafts })(Backlog)

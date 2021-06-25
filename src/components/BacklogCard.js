b619b47d4949c54afbbad63fede1f6bdedd8a67b
Michelle Ranly <meranly8@gmail.com>
2021-06-25 00:03:28 -0500

    Added description to BacklogCard

--- src/components/BacklogCard.js
+++ src/components/BacklogCard.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Backlog = (props) => {    
     return(
         <div>
-            <h5>
+            <h5 className="margin-0">
                 {props.craft.created_at}: {props.craft.name} - {props.craft.craft_type} &nbsp;
                 {window.location.pathname === "/crafts/backlog" ? (
                     <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button> 
                     ) : (
-                    null)
-                }
-            
+                    null
+                )}
             </h5>
+            {props.craft.description}
         </div>
     )
 }

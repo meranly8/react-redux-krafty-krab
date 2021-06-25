7f4216740b85d14756f34fb490a38ce0e1b8b412
Michelle Ranly <meranly8@gmail.com>
2021-06-25 01:38:25 -0500

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = (props) => {    
+    return(
+        <div>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <img src="https://i.imgur.com/kdw9Ixht.png" alt="kuddly-krab"/>
+            <img src="https://i.imgur.com/MsT2Q8Jt.jpg" alt="kuddly-sponge"/>
+            <img src="https://i.imgur.com/TqLeT3Ct.gif" alt="kool-krab"/>
+            <img src="https://i.imgur.com/CqP63Ewt.jpg" alt="kuddly-squid"/>
+            <img src="https://i.imgur.com/3Zfl0CVt.gif" alt="kowboy-krab"/><br />
+            <small>Michelle Ranly 2021</small>
+            
+        </div>
+    )
+}
+
+export default Footer

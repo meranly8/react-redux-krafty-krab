fb83aecda9254f0e22777a63551c4f95ffa9c269
Michelle Ranly <meranly8@gmail.com>
2021-06-25 23:39:22 -0500

    Moved backlog object out of renderBacklogCards()

--- src/containers/Backlog.js
+++ src/containers/Backlog.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import BacklogCard from '../components/BacklogCard'
+import BacklogCard from '../components/crafts/BacklogCard'
 
 class Backlog extends Component {
+    backlog = this.props.crafts.filter(craft => craft.backlog === true)
+    
     renderBacklogCards = () => {
-        const backlog = this.props.crafts.filter(craft => craft.backlog === true)
-        const sorted = backlog.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+        const sorted = this.backlog.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
 
         return sorted.map(craft => < BacklogCard key={craft.id} craft={craft}/>)
     }
 
     render() {
         return (
-            <div >
+            <div ><br />
                 <h3><u>Most Recent Ideas</u></h3>
-                <div >{this.renderBacklogCards()}</div>
+                <div>{this.renderBacklogCards()}</div>
             </div>
         )
     }

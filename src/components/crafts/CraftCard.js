90ed3c739d9aea41a036a6ddb6e0626ea63a94a3
Michelle Ranly <meranly8@gmail.com>
2021-06-26 02:07:31 -0500

    Added logic to display what craft's stage is on All Crafts CraftCard

--- src/components/crafts/CraftCard.js
+++ src/components/crafts/CraftCard.js
@@ -3,9 +3,21 @@ import { connect } from 'react-redux'
 import { deleteCraft } from '../../actions/craftActions'
 
 const CraftCard = props => {
+    const stage = () => {
+        if (props.craft.backlog === true) {
+            return "B"
+        } else if (props.craft.wip === true) {
+            return "WIP"
+        } else if (props.craft.inventory === true) {
+            return "I"
+        } else if (props.craft.sold === true) {
+            return "$"
+        }
+    }
+    
     return (
         <div>
-            <p>{props.craft.name}</p>
+            <p>{props.craft.name} <small>({stage()})</small></p>
         </div>
     )
 }

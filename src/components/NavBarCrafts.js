55a2316bb5d6464f29f93be4da1eaafe60dbb16f
Michelle Ranly <meranly8@gmail.com>
2021-06-25 23:31:53 -0500

    Reformatted spaces in Craft nav bar

--- src/components/NavBarCrafts.js
+++ src/components/NavBarCrafts.js
@@ -8,11 +8,11 @@ const NavBarCrafts = () => {
     
     return(
         <div className="nav-bar-craft">
-            <NavLink to="/crafts" exact style={style} activeClassName="current">All</NavLink>&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/backlog" style={style} activeClassName="current">Backlog</NavLink>&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/wip" style={style} activeClassName="current">Work In Progress</NavLink>&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/inventory" style={style} activeClassName="current">Inventory</NavLink>&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/sold" style={style} activeClassName="current">Sold</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts" exact style={style} activeClassName="current">All</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/backlog" style={style} activeClassName="current">Backlog</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/wip" style={style} activeClassName="current">WIP</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/inventory" style={style} activeClassName="current">Inventory</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/sold" style={style} activeClassName="current">Sold</NavLink>
         </div>
     )
 }

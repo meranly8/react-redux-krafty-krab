d87dee6632b8654f8c2a0b48ac3c38ec8dc4f8a7
Michelle Ranly <meranly8@gmail.com>
2021-06-26 01:45:53 -0500

    Moved date_sold to top of SoldCard

--- src/components/crafts/SoldCard.js
+++ src/components/crafts/SoldCard.js
@@ -5,13 +5,13 @@ import { deleteCraft } from '../../actions/craftActions'
 const SoldCard = (props) => {    
     return(
         <div>
+            Sold on {props.craft.date_sold}
             <h5 className="margin-0">
                 {props.craft.name} (${props.craft.price}) - {props.craft.craft_type} &nbsp;
                 
                 <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button> 
             </h5>
             {props.craft.description}<br />
-            Sold on {props.craft.date_sold} 
         </div>
     )
 }

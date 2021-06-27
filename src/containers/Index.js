02eb5370407692a6c0a7d2a2554ff1e4fbe6f2df
Michelle Ranly <meranly8@gmail.com>
2021-06-27 03:20:26 -0500

    Added Router with Routes & imported NavBarIndex

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchCrafts } from '../actions/craftActions'
 
-class Gallery extends Component {
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import NavBarIndex from '../components/NavBarIndex'
+
+class Index extends Component {
     componentDidMount() {
         this.props.fetchCrafts()
     }
@@ -10,7 +13,19 @@ class Gallery extends Component {
     render() {
         return (
             <div>
-                <h2>Index</h2>
+                <Router>
+                    <h2 className="main-header">Index</h2>
+                    < NavBarIndex /><br />
+                    
+                    < Switch >
+                        < Route exact path="/crafts/index/type">
+                            <h2>By Type</h2>
+                        </ Route >
+                        < Route exact path="/crafts/index/stage">
+                            <h2>By Stage</h2>
+                        </ Route >
+                    </ Switch >
+                </Router>
             </div>
         )
     }
@@ -22,4 +37,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { fetchCrafts })(Gallery)
+export default connect(mapStateToProps, { fetchCrafts })(Index)

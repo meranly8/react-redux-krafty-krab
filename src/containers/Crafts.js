d047ef6845498a65bb1882483ac0d404f3eaee34
Michelle Ranly <meranly8@gmail.com>
2021-06-27 00:37:28 -0500

    Defined renderBacklogCards(), renderWIPCards(), renderInventoryCards(), renderSoldCards() & updated Routes

--- src/containers/Crafts.js
+++ src/containers/Crafts.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { fetchCrafts } from '../actions/craftActions'
-import CraftCard from '../components/crafts/CraftCard'
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBarCrafts from '../components/NavBarCrafts'
+
+import CraftCard from '../components/crafts/CraftCard'
 import BacklogCard from '../components/crafts/BacklogCard'
+import WIPCard from '../components/crafts/WIPCard'
+import InventoryCard from '../components/crafts/InventoryCard'
 import SoldCard from '../components/crafts/SoldCard'
 
 class Crafts extends Component {
@@ -17,24 +21,30 @@ class Crafts extends Component {
         const sorted = craftsByType.sort((a, b) => a.name > b.name ? 1 : -1)
         return sorted.map(craft => <CraftCard key={craft.id} craft={craft}/>)
     }
-
-    renderCraftsByStage = stage => {
-        const craftsByStage = this.props.crafts.filter(craft => craft[stage] === true)
-        
-        return craftsByStage.map(craft => <CraftCard key={craft.id} craft={craft}/>)
-    }
+    
+    stageCrafts = (stage) => this.props.crafts.filter(craft => craft[stage] === true)
 
     renderBacklogCards = () => {
-        const backlog = this.props.crafts.filter(craft => craft["backlog"] === true)
-        const sorted = backlog.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
-        return sorted.map(craft => < BacklogCard key={craft.id} craft={craft}/>)
+        const sortedBacklog = this.stageCrafts('backlog').sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+        return sortedBacklog.map(craft => < BacklogCard key={craft.id} craft={craft}/>)
+    }
+
+    renderWIPCards = () => {
+        const sortedWIP = this.stageCrafts('wip').sort((a, b) => a.date_started > b.date_started ? 1 : -1)
+        return sortedWIP.map(craft => < WIPCard key={craft.id} craft={craft}/>)
+    }
+
+    renderInventoryCards = () => {
+        const sortedInventory = this.stageCrafts('inventory').sort((a, b) => a.date_completed > b.date_completed ? 1 : -1)
+        return sortedInventory.map(craft => < InventoryCard key={craft.id} craft={craft}/>)
     }
 
     renderSoldCards = () => {
-        const sold = this.props.crafts.filter(craft => craft["sold"] === true)
-        const sorted = sold.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
-        return sorted.map(craft => < SoldCard key={craft.id} craft={craft}/>)
+        const sortedSold = this.stageCrafts('sold').sort((a, b) => a.date_sold > b.date_sold ? 1 : -1)
+        return sortedSold.map(craft => < SoldCard key={craft.id} craft={craft}/>)
     }
+
+    
 
     render(){
         return (
@@ -75,17 +85,27 @@ class Crafts extends Component {
                         </ Route >
                         < Route exact path="/crafts/backlog">
                             <div >
-                                <h3><u>Ideas</u></h3>
-                                {this.renderBacklogCards()}
+                                <h3><u>Ideas:</u> {this.renderBacklogCards().length}</h3>
+                                <div>{this.renderBacklogCards()}</div>
                             </div>
                         </Route>
-                        < Route exact path="/crafts/wip">{this.renderCraftsByStage("wip")}</Route>
-                        < Route exact path="/crafts/inventory">{this.renderCraftsByStage("inventory")}</Route>
+                        < Route exact path="/crafts/wip">
+                            <div>
+                                <h3><u>Work in Progress:</u> {this.renderWIPCards().length}</h3>
+                                <div>{this.renderWIPCards()}</div>
+                            </div>
+                        </Route>
+                        < Route exact path="/crafts/inventory">
+                            <div>
+                                <h3><u>Inventory:</u> {this.renderInventoryCards().length}</h3>
+                                <div>{this.renderInventoryCards()}</div>
+                            </div>
+                        </Route>
                         < Route exact path="/crafts/sold">
                             <div>
                                 <h3 className="margin-bottom-0"><u>Crafts Sold:</u> {this.renderSoldCards().length}</h3>
                                 <h4 className="margin-0">Total: ${this.props.total}</h4><br />
-                                <div className="grid-container">{this.renderSoldCards()}</div>
+                                <div>{this.renderSoldCards()}</div>
                             </div>
                         </Route>
                     </ Switch >

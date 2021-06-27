aaf79c3db7a9c9c5b9e790a2dfb9e8ff8db39ec1
Michelle Ranly <meranly8@gmail.com>
2021-06-27 00:22:12 -0500

    Put delete <button> into header & added Date Added to render()

--- src/components/crafts/BacklogCard.js
+++ src/components/crafts/BacklogCard.js
@@ -5,15 +5,16 @@ import { deleteCraft } from '../../actions/craftActions'
 const BacklogCard = (props) => {
     return(
         <div>
-            <h5 className="margin-bottom-0">
-                {props.craft.created_at}: {props.craft.name} - {props.craft.craft_type} &nbsp;
-            </h5>
+            <h4 className="margin-bottom-0">
+                 {props.craft.name} - {props.craft.craft_type} &nbsp;
+                {window.location.pathname === "/crafts/backlog" ? (
+                    <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button>
+                    ) : (
+                    null
+                )}
+            </h4>
+            <p className="margin-0">Date Added: {props.craft.created_at}</p>
             {props.craft.description}
-            {window.location.pathname === "/crafts/backlog" ? (
-                <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button>
-                ) : (
-                null
-            )}
         </div>
     )
 }

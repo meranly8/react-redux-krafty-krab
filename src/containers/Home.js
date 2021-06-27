824062f5d74df0faa1b244392e8d4675572aaeae
Michelle Ranly <meranly8@gmail.com>
2021-06-26 21:49:40 -0500

    Defined renderBacklogCards() & called in render()

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Backlog from './Backlog'
+import BacklogCard from '../components/crafts/BacklogCard'
 import { fetchCrafts } from '../actions/craftActions'
 
 class Home extends Component {
     componentDidMount() {
         this.props.fetchCrafts()
     }
-
+    
     stage = (stage) => this.props.crafts.filter(craft => craft[stage] === true)
+
+    renderBacklogCards = () => {
+        const backlog = this.stage("backlog")
+        const sorted = backlog.sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+        return sorted.map(craft => < BacklogCard key={craft.id} craft={craft}/>)
+    }
 
     render(){
         return (
             <div className="grid-container-home"><br />
                 <section className="section-1">
-                    < Backlog />
+                    <h3><u>Most Recent Ideas</u></h3>
+                    {this.renderBacklogCards()}
                 </section>
                 <section>
                     <h3><u>Craft Stage Totals</u></h3>

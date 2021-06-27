b4b7233ff9870a59d6a3541bb3e392d0f9e9862c
Michelle Ranly <meranly8@gmail.com>
2021-06-27 01:33:36 -0500

    Reformatted contents of BacklogCard

--- src/components/crafts/BacklogCard.js
+++ src/components/crafts/BacklogCard.js
@@ -6,15 +6,15 @@ const BacklogCard = (props) => {
     return(
         <div>
             <h4 className="margin-bottom-0">
-                 {props.craft.name} - {props.craft.craft_type} &nbsp;
+                 {props.craft.name} ({props.craft.craft_type}) &nbsp;
                 {window.location.pathname === "/crafts/backlog" ? (
                     <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button>
                     ) : (
                     null
                 )}
             </h4>
-            <p className="margin-0">Date Added: {props.craft.created_at}</p>
-            {props.craft.description}
+            <small><p className="margin-0"><b>Date Added:</b> {props.craft.created_at}</p></small>
+            <small>{props.craft.description}</small>
         </div>
     )
 }

2dfaef8eb0e5d56fbdb0884e9442ed01e560f46c
Michelle Ranly <meranly8@gmail.com>
2021-06-27 03:05:53 -0500

    Created Index class container component

---
+++ src/containers/Index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCrafts } from '../actions/craftActions'
+
+class Gallery extends Component {
+    componentDidMount() {
+        this.props.fetchCrafts()
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Index</h2>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        crafts: state.crafts
+    }
+}
+
+export default connect(mapStateToProps, { fetchCrafts })(Gallery)

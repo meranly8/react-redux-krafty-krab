37773d568cac5942a838f708d242a61a2a089e31
Michelle Ranly <meranly8@gmail.com>
2021-06-27 01:35:33 -0500

    Reformatted contents of WIPCard

--- src/components/crafts/WIPCard.js
+++ src/components/crafts/WIPCard.js
@@ -6,12 +6,13 @@ const WIPCard = (props) => {
     return(
         <div>
             <h4 className="margin-0">
-                {props.craft.name} - {props.craft.craft_type} <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button>
+                {props.craft.name} ({props.craft.craft_type}) <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button>
             </h4>
-            <small>Date Started: {props.craft.date_started}</small><br />
-            <small>Days Since Started: {props.daysSince(props.craft.date_started)}</small><br />
+            <small><p className="margin-0"><b>Date Started:</b> {props.craft.date_started}</p>
+
+            <p className="margin-0"><b>Days Since Started:</b> {props.daysSince(props.craft.date_started)}</p></small>
             
-            {props.craft.description}<br /><br />
+            <small>{props.craft.description}</small><br /><br /><br />
         </div>
     )
 }

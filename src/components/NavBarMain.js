616ada7eb8889569848a8ae3c36afc6f66743abb
Michelle Ranly <meranly8@gmail.com>
2021-06-27 03:04:29 -0500

    Reordered NavBarMain NavLinks

--- src/components/NavBarMain.js
+++ src/components/NavBarMain.js
@@ -8,9 +8,9 @@ const NavBarMain = () => {
             <br />
             <NavLink to="/" exact className="link-main" activeClassName="current-main">Home</NavLink><br /><br />
             <NavLink to="/crafts" exact className="link-main" activeClassName="current-main">Crafts</NavLink><br /><br />
+            <NavLink to="/crafts/new" exact className="link-main" activeClassName="current-main">Add Craft</NavLink><br /><br />
             <NavLink to="/crafts/gallery" exact className="link-main" activeClassName="current-main">Gallery</NavLink><br /><br />
             <NavLink to="/crafts/index" exact className="link-main" activeClassName="current-main">Index</NavLink><br /><br />
-            <NavLink to="/crafts/new" exact className="link-main" activeClassName="current-main">Add Craft</NavLink><br /><br />
         </section>
     )
 }

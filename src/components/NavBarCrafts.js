ee18bd9ed1022232cb61c51508228a42656b2ea9
Michelle Ranly <meranly8@gmail.com>
2021-06-27 02:50:37 -0500

    Updated className of NavLinks for stlying

--- src/components/NavBarCrafts.js
+++ src/components/NavBarCrafts.js
@@ -2,17 +2,14 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const NavBarCrafts = () => {
-    const style = {
-        textDecoration: 'none', color: 'dark gray'
-    }
     
     return(
         <div className="nav-bar-craft">
-            <NavLink to="/crafts" exact style={style} activeClassName="current">All</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/backlog" style={style} activeClassName="current">Backlog</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/wip" style={style} activeClassName="current">WIP</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/inventory" style={style} activeClassName="current">Inventory</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <NavLink to="/crafts/sold" style={style} activeClassName="current">Sold</NavLink>
+            <NavLink to="/crafts" exact className="link" activeClassName="current">All</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/backlog" className="link" activeClassName="current">Backlog</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/wip" className="link" activeClassName="current">WIP</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/inventory" className="link" activeClassName="current">Inventory</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <NavLink to="/crafts/sold" className="link" activeClassName="current">Sold</NavLink>
         </div>
     )
 }

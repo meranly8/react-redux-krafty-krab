3aa094679fbb1e2fc44c9369e2da232ef20fe67c
Michelle Ranly <meranly8@gmail.com>
2021-06-27 01:34:58 -0500

    Reformatted contents of SoldCard

--- src/components/crafts/SoldCard.js
+++ src/components/crafts/SoldCard.js
@@ -5,12 +5,17 @@ import { deleteCraft } from '../../actions/craftActions'
 const SoldCard = (props) => {
     return(
         <div>
-            Sold on {props.craft.date_sold}
-            <h5 className="margin-0">
-                {props.craft.name} (${props.craft.price}) - {props.craft.craft_type} &nbsp;
-            </h5>
-            {props.craft.description}<br />
+            <h4 className="margin-0">
+                {props.craft.name} ({props.craft.craft_type})
+            </h4>
+            <small><p className="margin-0"><b>Date Sold:</b> {props.craft.date_sold}</p></small>
+
+            <small><p className="margin-0"><b>Sold For:</b> ${props.craft.price}</p></small><br />
+            
+            <small>{props.craft.description}</small><br />
+
             {props.craft.image_url === null || props.craft.image_url === "" ? null : <img src={props.craft.image_url} alt={props.craft.name} /> }<br />
+
             <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button><br/><br/><br/>
         </div>
     )

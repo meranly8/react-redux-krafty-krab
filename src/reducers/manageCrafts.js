b4aadc38f65636b7521a35d2bbc254d19e376a9a
Michelle Ranly <meranly8@gmail.com>
2021-06-26 21:53:36 -0500

    Updated DELETED_CRAFT to calculate new total for state

--- src/reducers/manageCrafts.js
+++ src/reducers/manageCrafts.js
@@ -14,7 +14,12 @@ const manageCrafts = (state = {crafts: [], loading: false, total: 0}, action) =>
             return {...state, crafts: [...state.crafts, action.payload] }
         case 'DELETED_CRAFT':
             const newCrafts = state.crafts.filter(craft => craft.id !== action.payload)
-            return {...state, crafts: newCrafts}
+
+            const newSold = newCrafts.filter(craft => craft.sold === true)
+            const newPrices = newSold.map(craft => craft.price)
+            const newSum = newPrices.reduce((total, price) => total + price)
+
+            return {...state, crafts: newCrafts, total: newSum}
         default:
             return state
     }

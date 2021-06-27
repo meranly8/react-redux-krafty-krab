78912c8ab411250360b922e123a610ccb289c09a
Michelle Ranly <meranly8@gmail.com>
2021-06-27 01:34:16 -0500

    Reformatted contents of InventoryCard

--- src/components/crafts/InventoryCard.js
+++ src/components/crafts/InventoryCard.js
@@ -5,16 +5,18 @@ import { deleteCraft } from '../../actions/craftActions'
 const InventoryCard = (props) => {
     return(
         <div>
-            
             <h4 className="margin-0">
-                {props.craft.name} - {props.craft.craft_type} &nbsp; <button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button>
+                {props.craft.name} ({props.craft.craft_type})
             </h4>
-            <small><p className="margin-bottom-0"><b>Date Started:</b> {props.craft.date_started}</p> 
+            <small><p className="margin-0"><b>Date Started:</b> {props.craft.date_started}</p> 
+
             <p className="margin-0"><b>Date Completed:</b> {props.craft.date_completed}</p>
+
             <p className="margin-0"><b>Days to Complete:</b> {props.daysSince(props.craft.date_started, props.craft.date_completed)}</p></small><br />
-            {props.craft.description}<br />
-            {props.craft.image_url === null || props.craft.image_url === "" ? null : <img src={props.craft.image_url} alt={props.craft.name} /> }<br />
-            
+
+            <small>{props.craft.description}</small><br />
+
+            {props.craft.image_url === null || props.craft.image_url === "" ? null : <img src={props.craft.image_url} alt={props.craft.name} /> }<br /><button onClick={() => props.deleteCraft(props.craft.id)}>Delete</button><br /><br /><br />
         </div>
     )
 }

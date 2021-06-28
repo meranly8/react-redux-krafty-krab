df5b8a7b94b95c43b9d090ca1d8e4366651b8900
Michelle Ranly <meranly8@gmail.com>
2021-06-28 00:47:21 -0500

    Added 'image-footer' class to <img>s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
     return(
         <div>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <img src="https://i.imgur.com/MsT2Q8Jt.jpg" alt="kuddly-sponge"/>
-            <img src="https://i.imgur.com/kdw9Ixht.png" alt="kuddly-krab"/>
-            <img src="https://i.imgur.com/CqP63Ewt.jpg" alt="kuddly-squid"/><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <img src="https://i.imgur.com/MsT2Q8Jt.jpg" alt="kuddly-sponge" className="image-footer"/>
+            <img src="https://i.imgur.com/kdw9Ixht.png" alt="kuddly-krab" className="image-footer"/>
+            <img src="https://i.imgur.com/CqP63Ewt.jpg" alt="kuddly-squid" className="image-footer"/><br />
             <small>&copy; mranly 2021</small>
         </div>
     )

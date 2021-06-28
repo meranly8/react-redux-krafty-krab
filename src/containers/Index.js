2c165c2cfd329d7cadc0510bb8bc1580b929c022
Michelle Ranly <meranly8@gmail.com>
2021-06-28 01:02:33 -0500

    Removed CraftsByType; defined renderCraftCards() & added to render()

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchCrafts } from '../actions/craftActions'
 
-import CraftsByType from '../components/crafts/CraftsByType'
+import CraftCardFull from '../components/crafts/CraftCardFull'
 
 class Index extends Component {
     componentDidMount() {
         this.props.fetchCrafts()
     }
 
+    renderCraftCards = () => {
+        const crafts = this.props.crafts.sort((a, b) => a.name > b.name ? 1 : -1)
+        return crafts.map(craft => < CraftCardFull key={craft.id} craft={craft}/>)
+    }
+
     render() {
         return (
             <div>
-                <h2 className="main-header">Index</h2>
+                <h2 className="main-header">Index ({this.renderCraftCards().length})</h2>
                 <div className="padding-left">
                     <div>
-                        < CraftsByType crafts={this.props.crafts}/>
+                        <br />
+                        {this.renderCraftCards()}
                     </div>
                 </div>
             </div>

7c0877e8e19ae5a46c807975db77bc7577520f09
Michelle Ranly <meranly8@gmail.com>
2021-06-27 21:14:12 -0500

    Refined Index NavLink path to /crafts/index

--- src/components/NavBarMain.js
+++ src/components/NavBarMain.js
@@ -10,7 +10,7 @@ const NavBarMain = () => {
             <NavLink to="/crafts" exact className="link-main" activeClassName="current-main">Crafts</NavLink><br /><br />
             <NavLink to="/crafts/new" exact className="link-main" activeClassName="current-main">Add Craft</NavLink><br /><br />
             <NavLink to="/crafts/gallery" exact className="link-main" activeClassName="current-main">Gallery</NavLink><br /><br />
-            <NavLink to="/crafts/index/type" exact className="link-main" activeClassName="current-main">Index</NavLink><br /><br />
+            <NavLink to="/crafts/index" exact className="link-main" activeClassName="current-main">Index</NavLink><br /><br />
         </section>
     )
 }

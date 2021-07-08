cbf75a3fa837383af0701e5d0b11cc00aa35db40
Michelle Ranly <meranly8@gmail.com>
2021-07-07 23:59:56 -0400

    Imported CraftImage & implemented incrementable 'like' functionality

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchCrafts } from '../actions/craftActions'
 
+import CraftImage from '../components/crafts/CraftImage'
+
 class Gallery extends Component {
+    state = {
+        incrementBy: 0
+    }
+    
     componentDidMount() {
         this.props.fetchCrafts()
     }
 
     renderImages = () => {
         const images = this.props.crafts.filter(craft => craft.image_url !== null)
-        return images.map(craft => {
-            return (
-                <img src={craft.image_url} alt={craft.name} key={craft.id} className="image-gallery"/>
-            )
-        })
+        return images.map(craft => <CraftImage craft={craft} incrementBy={this.state.incrementBy} key={craft.id}/>)
+    }
+    
+    incrementNumber = event => {
+        this.setState({
+            incrementBy: event.target.value
+        }, () => console.log(this.state))
     }
     
     render() {
@@ -21,6 +29,7 @@ class Gallery extends Component {
             <div className="padding-left">
                 <br /><br /><br />
                 <div className="grid-container-crafts">
+                    <input type="number" onChange={this.incrementNumber}/><br/>
                     {this.renderImages()}
                 </div>
                 <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
